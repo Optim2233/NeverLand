d28ca00b30b9edca188bd010fc5a798bc411c1d9
Optim2233 <sunandmoon8484984@gmail.com>
2025-10-02 10:39:59 +0630

    f

---
+++ new.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+<<<<<<< Updated upstream
+int main()
+{
+    cout<<"Hello world";
+=======
+
+int main()
+{
+    cout<<"Testing"<<endl;
+    return 0;
+>>>>>>> Stashed changes
+}
