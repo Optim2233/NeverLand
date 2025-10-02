71996cc32199a9456ed63572c8ae9365682628f0
Optim2233 <sunandmoon8484984@gmail.com>
2025-10-02 10:40:14 +0630

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 int main()
 {
     cout<<"Hello world";
 =======
+=======
+>>>>>>> Stashed changes
 
 int main()
 {
     cout<<"Testing"<<endl;
     return 0;
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 }
