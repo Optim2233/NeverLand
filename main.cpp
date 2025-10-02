b8f2a3baee7a8bfdf67504e2761a20867aabc223
Optim2233 <sunandmoon8484984@gmail.com>
2025-10-02 10:26:31 +0630

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+class Distance 
+{
+    private: int feet; int in;
+    public: 
+    Distance(int f, int i) : feet(f) , in(i){std::cout<<feet<<" "<<in;}
+};
 int main() {
     std::cout << "Hello from NeverLand!" << std::endl;
+    Distance(4,2);
     return 0;
 }
